b3dfdfecfbb2ad571edede77f3bb8eb46559b0f9
Illyria <amasumig@hotmail.com>
2021-09-14 17:54:46 -0400

    Added Start Button to Main Scene

---
+++ Assets/Scripts/StartButtonBehavior.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonBehavior : MonoBehaviour
+{
+    
+    public void OnStartButtonPressed()
+    {
+        Debug.Log("Start Button Pressed!");
+    }
+
+}
